4a7a93074106a71531fcb2045e18e134a8113171
rronaldr <ronaldrronaldr@gmail.com>
2017-12-18 23:21:04 +0100

    Added treatment notification, with treatment checkout activity

--- app/src/main/java/com/ronald/medin/TreatmentActivity.java
+++ app/src/main/java/com/ronald/medin/TreatmentActivity.java
@@ -16,7 +16,7 @@ public class TreatmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_treatment_result);
-        
+
 
         String x;
         TextView displMsg = (TextView) this.findViewById(R.id.msgTextView);
